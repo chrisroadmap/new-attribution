8643a88b24c8e8759cca5e30e5ccfa39c351351d
Chris Smith <chrisroadmap@gmail.com>
2025-02-20 01:07:12 +0100

    wip

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # new-attribution
 Attribution of 2023 and 2024 warming
+
+## emissions counterfactual
+new dataset prepared for ScenarioMIP CMIP7, with constant emissions of all GHGs and SLCFs from 2019 to 2024.
+
+## solar maximum
+counterfactual: IGCC with zero solar forcing from the point where the ERF crosses zero, circa 2022
+experiment: IGCC
+
+## Hunga Tonga
+counterfactual: IGCC with HTHH removed in 2023, 2024 and 2025 timebounds
+- stratospheric water vapour easy to back out as is present in IGCC time series and nothing else really affected stratospheric water vapour
+- sulphate more difficult, but I have a method to isolate it from the total SAOD time series
+  - look at monthly SAOD prepared for IGCC, from GloSSAC, and assume that HTHH was only significant eruption between Dec 2021 and Apr 2022
+  - we see that SAOD increased from 0.008 to 0.015 (about 0.007 units) peaking in Apr 2022
+  - assume that SAOD anomaly decays away with an e-folding lifetime of 24 months starting from t=0 in Apr 2022. This is conservative, since large eruptions (is HTHH "large"?) decay away with an e-folding lifetime more like 16 months, see https://acp.copernicus.org/articles/23/921/2023/
+  - calculate ERF contribution based on -20 x SAOD + WV (-20 from AR6, should be sampled, as should the other uncertainties)
+
+A back of the envelope sense check for the 0.007 AOD comes from:
+- total SO4 burden increase 0.66 Tg (https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2023GL105076)
+- AOD per TgSO4, in a SRM context (couldn't find a paper on volcanoes, quickly) is 0.004-0.02 AOD/Tg depending on latitude and injection rate (fig 2d, https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2023GL104417). Taking 0.01 as a mid-range guess and multiply 0.066 x 0.01 = 0.0066 AOD units (rounds to 0.007). Fig. 2c in this paper also gives another line of evidence for a 16 month lifetime.
+
+experiment: IGCC (includes HTHH)
--- scripts/02_run-counterfactual.py
+++ scripts/02_run-counterfactual.py
@@ -26,25 +26,41 @@ from fair.interface import fill, initialise
 from fair.io import read_properties
 
 # %%
-scenarios = ['counterfactual']
+# temperature rebasing: only used for reporting here
+weights = np.ones(52)
+weights[0] = 0.5
+weights[-1] = 0.5
+
+# %%
+# output arrays
+temp_out = np.ones((276, 2, 841)) * np.nan
+faer_out = np.ones((276, 2, 841)) * np.nan
+fnat_out = np.ones((276, 2, 841)) * np.nan
+fant_out = np.ones((276, 2, 841)) * np.nan
+
+# %%
+# full scenario names
+labels = ['counterfactual', 'counterfactual_internal_variability']
 
 # %%
 df_configs = pd.read_csv('../data/fair-parameters/fair-calibrate-1.5.0/calibrated_constrained_parameters.csv', index_col=0)
 
 # %%
-df_solar = pd.read_csv('../data/forcing/solar_erf_timebounds_2022zero.csv', index_col=0)
-df_volcanic = pd.read_csv('../data/forcing/volcanic_erf_timebounds_2020_constant.csv', index_col=0)
+df_solar = pd.read_csv('../data/forcing/solar_erf_timebounds_1750-2025.csv', index_col=0)
+df_volcanic = pd.read_csv('../data/forcing/volcanic_erf_timebounds_1750-2025.csv', index_col=0)
+
+# %%
+df_solar['counterfactual'].values
+
+# %% [markdown]
+# ## Counterfactual: no internal variability
+
+# %%
+scenarios = ['counterfactual']
 
 # %%
 # this could be parallel - let's see how painful in serial
 # define problem
-
-temp_out = np.ones((276, len(scenarios), 841)) * np.nan
-faer_out = np.ones((276, len(scenarios), 841)) * np.nan
-labels = []
-weights = np.ones(52)
-weights[0] = 0.5
-weights[-1] = 0.5
 
 for iscen, scenario in enumerate(tqdm(scenarios)):
     f = FAIR(ch4_method="Thornhill2021")
@@ -58,8 +74,8 @@ for iscen, scenario in enumerate(tqdm(scenarios)):
     f.fill_from_csv('../data/emissions/historical_emissions_1750-2024.csv')
 
     # fill external forcings
-    fill(f.forcing, df_solar.values[:, None] * df_configs['forcing_scale[Solar]'].values[None, :], specie='Solar')
-    fill(f.forcing, df_volcanic.values[:, None] * df_configs['forcing_scale[Volcanic]'].values[None, :], specie='Volcanic')
+    fill(f.forcing, df_solar['counterfactual'].values[:, None, None] * df_configs['forcing_scale[Solar]'].values[None, None, :], specie='Solar')
+    fill(f.forcing, df_volcanic['counterfactual'].values[:, None, None] * df_configs['forcing_scale[Volcanic]'].values[None, None :], specie='Volcanic')
 
     # override_defaults is very slow so read in xarray directly
     ds_sc = xr.load_dataset("../output/calibration_binaries/fair-calibrate-1.5.0/species_configs.nc")
@@ -77,22 +93,79 @@ for iscen, scenario in enumerate(tqdm(scenarios)):
     f.run(progress=False)
     temp_out[:, iscen, :] = f.temperature[:, 0, :, 0]# - np.average(f.temperature[100:152, 0, :, 0], weights=weights, axis=0)
     faer_out[:, iscen, :] = f.forcing[:, 0, :, 54] + f.forcing[:, 0, :, 55]
-    labels.append(scenario)
+    fnat_out[:, iscen, :] = f.forcing[:, 0, :, 52] + f.forcing[:, 0, :, 53]
+    fant_out[:, iscen, :] = f.forcing_sum[:, 0, :] - fnat_out[:, iscen, :]
 
 # %%
-pl.plot(np.median(temp_out[:, 0, :], axis=1));
-
-# %%
-np.median(temp_out[:, 0, :], axis=1)
+pl.plot(np.median(temp_out[:, 0, :] - np.average(f.temperature[100:152, 0, :, 0], weights=weights, axis=0), axis=1));
 
 # %%
 os.makedirs('../output/results', exist_ok=True)
+
+# %%
+np.median(temp_out[250:, 0, :] - np.average(f.temperature[100:152, 0, :, 0], weights=weights, axis=0), axis=1)
+
+# %% [markdown]
+# ## Counterfactual, with internal variability
+
+# %%
+scenarios = ['counterfactual']
+
+# %%
+# this could be parallel - let's see how painful in serial
+# define problem
+
+for iscen, scenario in enumerate(tqdm(scenarios)):
+    f = FAIR(ch4_method="Thornhill2021")
+    f.define_time(1750, 2025, 1)
+    f.define_scenarios([scenario])
+    f.define_configs(df_configs.index)
+    species, properties = read_properties('../data/fair-parameters/fair-calibrate-1.5.0/species_configs_properties.csv')
+    f.define_species(species, properties)
+    f.allocate()
+
+    f.fill_from_csv('../data/emissions/historical_emissions_1750-2024.csv')
+
+    # fill external forcings
+    fill(f.forcing, df_solar['counterfactual'].values[:, None, None] * df_configs['forcing_scale[Solar]'].values[None, None, :], specie='Solar')
+    fill(f.forcing, df_volcanic['counterfactual'].values[:, None, None] * df_configs['forcing_scale[Volcanic]'].values[None, None, :], specie='Volcanic')
+
+    # override_defaults is very slow so read in xarray directly
+    ds_sc = xr.load_dataset("../output/calibration_binaries/fair-calibrate-1.5.0/species_configs.nc")
+    ds_ebm = xr.load_dataset("../output/calibration_binaries/fair-calibrate-1.5.0/energy_balance_parameters.nc")
+    f.species_configs = ds_sc
+    f.climate_configs = ds_ebm
+
+    # turn on internal variability this time
+    fill(f.climate_configs['stochastic_run'], True)
+    fill(f.climate_configs['use_seed'], True)
+
+    initialise(f.concentration, f.species_configs["baseline_concentration"])
+    initialise(f.forcing, 0)
+    initialise(f.temperature, 0)
+    initialise(f.cumulative_emissions, 0)
+    initialise(f.airborne_emissions, 0)
+    initialise(f.ocean_heat_content_change, 0)
+    
+    f.run(progress=False)
+    temp_out[:, 1, :] = f.temperature[:, 0, :, 0]
+    faer_out[:, 1, :] = f.forcing[:, 0, :, 54] + f.forcing[:, 0, :, 55]
+    fnat_out[:, 1, :] = f.forcing[:, 0, :, 52] + f.forcing[:, 0, :, 53]
+    fant_out[:, 1, :] = f.forcing_sum[:, 0, :] - fnat_out[:, 1, :]
+
+# %%
+pl.plot(np.median(temp_out[:, 1, :] - np.average(f.temperature[100:152, 0, :, 0], weights=weights, axis=0), axis=1));
+
+# %%
+np.median(temp_out[250:, 1, :] - np.average(f.temperature[100:152, 0, :, 0], weights=weights, axis=0), axis=1)
 
 # %%
 ds = xr.Dataset(
     data_vars = dict(
         temperature_anomaly_rel_1750 = (['timebound', 'scenario', 'config'], temp_out),
         aerosol_forcing = (['timebound', 'scenario', 'config'], faer_out),
+        natural_forcing = (['timebound', 'scenario', 'config'], fnat_out),
+        anthropogenic_forcing = (['timebound', 'scenario', 'config'], fant_out),
     ),
     coords = dict(
         timebound = np.arange(1750, 2026),
