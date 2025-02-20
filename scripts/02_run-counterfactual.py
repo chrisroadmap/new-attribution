cf18b922b61cecd02fbde8ed5bdf85fd3b8553d1
Chris Smith <chrisroadmap@gmail.com>
2025-02-20 21:18:36 +0100

    WIP commit

--- scripts/02_run-counterfactual.py
+++ scripts/02_run-counterfactual.py
@@ -33,14 +33,18 @@ weights[-1] = 0.5
 
 # %%
 # output arrays
-temp_out = np.ones((276, 2, 841)) * np.nan
-faer_out = np.ones((276, 2, 841)) * np.nan
-fnat_out = np.ones((276, 2, 841)) * np.nan
-fant_out = np.ones((276, 2, 841)) * np.nan
+temp_out = np.ones((276, 3, 841)) * np.nan
+faer_out = np.ones((276, 3, 841)) * np.nan
+fnat_out = np.ones((276, 3, 841)) * np.nan
+fant_out = np.ones((276, 3, 841)) * np.nan
+
+# %%
+os.makedirs('../output/results', exist_ok=True)
 
 # %%
 # full scenario names
-labels = ['counterfactual', 'counterfactual_internal_variability']
+# labels = ['counterfactual', 'counterfactual_internal_variability']
+scenarios = ['counterfactual', 'hunga', 'solar']
 
 # %%
 df_configs = pd.read_csv('../data/fair-parameters/fair-calibrate-1.5.0/calibrated_constrained_parameters.csv', index_col=0)
@@ -49,61 +53,54 @@ df_configs = pd.read_csv('../data/fair-parameters/fair-calibrate-1.5.0/calibrate
 df_solar = pd.read_csv('../data/forcing/solar_erf_timebounds_1750-2025.csv', index_col=0)
 df_volcanic = pd.read_csv('../data/forcing/volcanic_erf_timebounds_1750-2025.csv', index_col=0)
 
-# %%
-df_solar['counterfactual'].values
-
 # %% [markdown]
 # ## Counterfactual: no internal variability
-
-# %%
-scenarios = ['counterfactual']
 
 # %%
 # this could be parallel - let's see how painful in serial
 # define problem
 
-for iscen, scenario in enumerate(tqdm(scenarios)):
-    f = FAIR(ch4_method="Thornhill2021")
-    f.define_time(1750, 2025, 1)
-    f.define_scenarios([scenario])
-    f.define_configs(df_configs.index)
-    species, properties = read_properties('../data/fair-parameters/fair-calibrate-1.5.0/species_configs_properties.csv')
-    f.define_species(species, properties)
-    f.allocate()
+f = FAIR(ch4_method="Thornhill2021")
+f.define_time(1750, 2025, 1)
+f.define_scenarios(scenarios)
+f.define_configs(df_configs.index)
+species, properties = read_properties('../data/fair-parameters/fair-calibrate-1.5.0/species_configs_properties.csv')
+f.define_species(species, properties)
+f.allocate()
 
-    f.fill_from_csv('../data/emissions/historical_emissions_1750-2024.csv')
+f.fill_from_csv('../data/emissions/historical_emissions_1750-2024.csv')
 
-    # fill external forcings
-    fill(f.forcing, df_solar['counterfactual'].values[:, None, None] * df_configs['forcing_scale[Solar]'].values[None, None, :], specie='Solar')
-    fill(f.forcing, df_volcanic['counterfactual'].values[:, None, None] * df_configs['forcing_scale[Volcanic]'].values[None, None :], specie='Volcanic')
+# fill external forcings
+for scenario in scenarios:
+    fill(f.forcing, df_solar[scenario].values[:, None] * df_configs['forcing_scale[Solar]'].values[None, :], specie='Solar', scenario=scenario)
+    fill(f.forcing, df_volcanic[scenario].values[:, None] * df_configs['forcing_scale[Volcanic]'].values[None, :], specie='Volcanic', scenario=scenario)
 
-    # override_defaults is very slow so read in xarray directly
-    ds_sc = xr.load_dataset("../output/calibration_binaries/fair-calibrate-1.5.0/species_configs.nc")
-    ds_ebm = xr.load_dataset("../output/calibration_binaries/fair-calibrate-1.5.0/energy_balance_parameters.nc")
-    f.species_configs = ds_sc
-    f.climate_configs = ds_ebm
+# override_defaults is very slow so read in xarray directly
+ds_sc = xr.load_dataset("../output/calibration_binaries/fair-calibrate-1.5.0/species_configs.nc")
+ds_ebm = xr.load_dataset("../output/calibration_binaries/fair-calibrate-1.5.0/energy_balance_parameters.nc")
+f.species_configs = ds_sc
+f.climate_configs = ds_ebm
 
-    initialise(f.concentration, f.species_configs["baseline_concentration"])
-    initialise(f.forcing, 0)
-    initialise(f.temperature, 0)
-    initialise(f.cumulative_emissions, 0)
-    initialise(f.airborne_emissions, 0)
-    initialise(f.ocean_heat_content_change, 0)
-    
-    f.run(progress=False)
-    temp_out[:, iscen, :] = f.temperature[:, 0, :, 0]# - np.average(f.temperature[100:152, 0, :, 0], weights=weights, axis=0)
-    faer_out[:, iscen, :] = f.forcing[:, 0, :, 54] + f.forcing[:, 0, :, 55]
-    fnat_out[:, iscen, :] = f.forcing[:, 0, :, 52] + f.forcing[:, 0, :, 53]
-    fant_out[:, iscen, :] = f.forcing_sum[:, 0, :] - fnat_out[:, iscen, :]
+initialise(f.concentration, f.species_configs["baseline_concentration"])
+initialise(f.forcing, 0)
+initialise(f.temperature, 0)
+initialise(f.cumulative_emissions, 0)
+initialise(f.airborne_emissions, 0)
+initialise(f.ocean_heat_content_change, 0)
 
-# %%
-pl.plot(np.median(temp_out[:, 0, :] - np.average(f.temperature[100:152, 0, :, 0], weights=weights, axis=0), axis=1));
+f.run(progress=False)
+temp_out[:, :, :] = f.temperature[:, :, :, 0]# - np.average(f.temperature[100:152, 0, :, 0], weights=weights, axis=0)
+faer_out[:, :, :] = f.forcing[:, :, :, 54] + f.forcing[:, :, :, 55]
+fnat_out[:, :, :] = f.forcing[:, :, :, 52] + f.forcing[:, :, :, 53]
+fant_out[:, :, :] = f.forcing_sum[:, :, :] - fnat_out[:, :, :]
 
 # %%
-os.makedirs('../output/results', exist_ok=True)
+pl.plot(np.median(temp_out[:, :, :] - np.average(f.temperature[100:152, :, :, 0], weights=weights, axis=0), axis=2));
 
 # %%
-np.median(temp_out[250:, 0, :] - np.average(f.temperature[100:152, 0, :, 0], weights=weights, axis=0), axis=1)
+np.median(temp_out[250:, :, :] - np.average(f.temperature[100:152, :, :, 0], weights=weights, axis=0), axis=2)
+
+# %%
 
 # %% [markdown]
 # ## Counterfactual, with internal variability
